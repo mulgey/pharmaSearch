6f56b6b68a9f4e3bff5497868c27d84b40de0bf9
mulgey <mustafaulgey@hotmail.com>
2020-02-21 16:10:14 +0300

    hard-week

--- test.js
+++ test.js
@@ -1,28 +1,68 @@
-// CLEAR THE SEARCH OBJECT / ADD LIST PARAMETERS (ACTSUBS) FOR NEW SEARCH
-searchObj = {
-title: "",
-filters: {
-    url: "",
-    retrospective: [2],
-    ward_focused: [2],
-    indication_focused: [2],
-    active_substance: [actSubs],
-}
-};
-// DESTROY ACTSUBS IF NONE IS SELECETED
-if (actSubs === "None") {
-searchObj['filters']['active_substance'] = "";
-}
-// MIRROR THE FULL DATA ON THE CONSOLE
-let myData = JSON.parse(dataReq.responseText);
-console.log(myData);
-// SET THE NEW SEARCH DATA ACCORDING TO THE FILTER DATA
-if (filterArr[0] === true) {
-searchObj['filters']['retrospective'] = [1];
-}
-if (filterArr[1] === true) {
-searchObj['filters']['ward_focused'] = [1];
-}
-if (filterArr[2] === true) {
-searchObj['filters']['indication_focused'] = [1];
-}
+script.
+    const selectedFilters = {
+    retrospective: ["1"],
+    wardfocused: [],
+    indicationfocused: [],
+    activesubstance: []
+    };
+
+    const items = [
+    {
+    title: "Real-world evidence of high-cost drugs for metastatic melanoma: effectiveness, compliance to clinical practice guidelines and economic evaluation",
+    url: "https://ejhp.bmj.com/content/26/Suppl_1/A5.1",
+    filters: {
+        retrospective: ["1"],
+        wardfocused: ["2"],
+        indicationfocused: ["1"],
+        activesubstance: ["2"]
+    }
+    },
+    {
+    title: "Real-world safety and tolerability of the recently commercialised palbociclib",
+    url: "https://ejhp.bmj.com/content/26/Suppl_1/A223.2",
+    filters: {
+        retrospective: ["2"],
+        wardfocused: ["1"],
+        indication_focused: ["2"],
+        activesubstance: ["Palbociclib"]
+    }
+    },
+    {
+    name: "Cost-effectiveness of morphine versus fentanyl in managing ventilated neonates with respiratory distress syndrome in the intensive care setting",
+    url: "https://ejhp.bmj.com/content/26/Suppl_1/A7.3",
+    filters: {
+        retrospective: ["1"],
+        wardfocused: ["2"],
+        indication_focused: ["1"],
+        activesubstance: ["2"]
+    }
+    },
+    {
+    name: "Chemical risk assessement in a quality control laboratory by a tool using activity analysis",
+    url: "https://ejhp.bmj.com/content/26/Suppl_1/A9.2",
+    filters: {
+        retrospective: ["1"],
+        wardfocused: ["2"],
+        indication_focused: ["1"],
+        activesubstance: ["2"]
+    }
+    },
+    {
+    name: "The economic burden of metastatic breast cancer in Spain",
+    url: "https://ejhp.bmj.com/content/27/1/19",
+    filters: {
+        retrospective: ["1"],
+        wardfocused: ["2"],
+        indication_focused: ["1"],
+        activesubstance: ["2"]
+    }
+    }
+    ];
+
+    const filterArr01 = Object.values(selectedFilters).flat();
+
+    const output = items.filter(({filters}) => {
+        const objFilter = Object.values(filters).flat();
+        return filterArr01.every(val => objFilter.includes(val));
+    })
+    console.log(output);
